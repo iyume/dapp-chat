23042739164c39b95a838c869f741634e813993e
iyume <iyumelive@gmail.com>
2023-05-04 23:53:05 +0800

    just experimental code

--- cmd/test_keystore/main.go
+++ cmd/test_keystore/main.go
@@ -2,27 +2,53 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 )
 
-func main() {
-	keyFile := "data/keystore/UTC--2023-04-18T12-49-23.162529799Z--d542be4551d114a7a2b544bafb7a9feba8784e69"
-	password := "123"
-
-	// 读取 keystore 文件
-	jsonBytes, err := os.ReadFile(keyFile)
+func testDecryptKey(keyfile string, passphrase string) {
+	// 读取 keystore 文件 (json bytes)
+	keyjson, err := os.ReadFile(keyfile)
 	if err != nil {
-		fmt.Println("Error reading keystore file:", err)
-		return
+		log.Fatalln("Error reading keystore file:", err)
 	}
-
 	// 解密 keystore 文件
-	key, err := keystore.DecryptKey(jsonBytes, password)
-	_ = key
+	key, err := keystore.DecryptKey(keyjson, passphrase)
 	if err != nil {
-		fmt.Println("Error decrypting keystore:", err)
-		return
+		log.Fatalln("Error decrypting keystore:", err)
 	}
+	fmt.Println("Decrypted key:", key)
+}
+
+func getKeyStore(keystoredir string, lightKDF bool) *keystore.KeyStore {
+	n, p := keystore.StandardScryptN, keystore.StandardScryptP
+	if lightKDF {
+		n, p = keystore.LightScryptN, keystore.LightScryptP
+	}
+	ks := keystore.NewKeyStore(keystoredir, n, p)
+	return ks
+}
+
+func getKeyStoreStandardKDF(keystoredir string) *keystore.KeyStore {
+	return getKeyStore(keystoredir, false)
+}
+
+func testFindByAddress(ks *keystore.KeyStore, address common.Address) {
+	// find by URL is also OK
+	account, err := ks.Find(accounts.Account{Address: address})
+	if err != nil {
+		log.Fatalln("Cannot find account:", err)
+	}
+	fmt.Printf("Keyfile for address %v is %v", address, account.URL)
+}
+
+func main() {
+	testDecryptKey("data/keystore/UTC--2023-04-18T12-49-23.162529799Z--d542be4551d114a7a2b544bafb7a9feba8784e69", "123")
+	ks := getKeyStoreStandardKDF("data/keystore")
+	fmt.Println("List all accounts:", ks.Accounts())
+	testFindByAddress(ks, common.HexToAddress("8cedb7c6af8a7781ec89bb84900768de99c8235b"))
 }
