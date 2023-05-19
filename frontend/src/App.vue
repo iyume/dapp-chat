74a84a5f9de354a818baf27c7d8abd658e09b979
iyume <iyumelive@gmail.com>
2023-05-19 22:03:57 +0800

    add Navbar, Sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Navbar from "./components/Navbar.vue";
+import Sidebar from "./components/Sidebar.vue";
+import { currentPage } from "./store";
+</script>
 
 <template>
-  <div class="bg-red-500">
+  <!-- <div class="bg-red-500">
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <button class="btn">Hello daisyUI</button>
-  </div>
+  </div> -->
+  <Navbar drawer-id="my-drawer-main" />
+  <Sidebar drawer-id="my-drawer-main">
+    <template #content>
+      <template v-if="currentPage == 'main'">
+        <p>this is main page</p>
+      </template>
+      <template v-if="currentPage == 'p2p'">
+        <p>this is p2p page</p>
+      </template>
+    </template>
+  </Sidebar>
 </template>
---
+++ frontend/src/store.ts
@@ -0,0 +1,3 @@
+import { ref } from "vue";
+
+export const currentPage = ref<"main" | "p2p">("main");
