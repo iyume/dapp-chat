bdc3d3a2b2aaceac7ecf62f3bf0501f9c47846fb
zhou u74a8 <1571224208@qq.com>
2023-05-22 11:07:41 +0800

    p2p page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import Navbar from "./components/Navbar.vue";
 import Sidebar from "./components/Sidebar.vue";
 import { currentPage } from "./store";
+import P2pView from "./views/P2pView.vue";
 </script>
 
 <template>
@@ -10,13 +11,13 @@ import { currentPage } from "./store";
     <button class="btn">Hello daisyUI</button>
   </div> -->
   <Navbar drawer-id="my-drawer-main" />
-  <Sidebar drawer-id="my-drawer-main">
+  <Sidebar drawer-id="my-drawer-main" class="h-[calc(100vh-4rem)]">
     <template #content>
       <template v-if="currentPage == 'main'">
         <p>this is main page</p>
       </template>
       <template v-if="currentPage == 'p2p'">
-        <p>this is p2p page</p>
+        <p2p-view />
       </template>
     </template>
   </Sidebar>
---
+++ frontend/src/views/P2pView.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="h-full">
+    <div
+      class="w-1/4 h-full float-left p-2 drawer-side bg-gray-100 overflow-y-auto"
+    >
+      <ul class="w-full menu menu-vertical">
+        <li v-for="fr in friends" :key="fr.node_id">
+          <div
+            @click="select_chat(fr.node_id)"
+            :class="chat_with == fr.node_id ? 'active' : ''"
+          >
+            <div class="avatar">
+              <div class="w-8 rounded">
+                <img :src="avatar" />
+              </div>
+            </div>
+            <span class="ml15">{{ fr.remark }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="w-3/4 h-full float-right p-2 drawer-content overflow-y-auto">
+      <div v-if="chat_with != null" class="h-full w-full">
+        <div class="h-3/4 w-full overflow-y-auto">
+          <div v-for="item in message_list" :key="item.message_id">
+            <div
+              :class="`chat ${item.direction == 1 ? 'chat-start' : 'chat-end'}`"
+            >
+              <div class="chat-image avatar">
+                <div class="w-10 rounded-full">
+                  <img :src="item.avatar" />
+                </div>
+              </div>
+              <div class="chat-header">
+                {{ item.name }}
+                <time class="text-xs opacity-50">{{ item.sendTime }}</time>
+              </div>
+              <div class="chat-bubble">{{ item.content }}</div>
+              <div class="opacity-50 chat-footer" v-if="item.direction == 2">
+                {{ item.seenTime }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container h-1/4 w-full form-control">
+          <textarea
+            class="textarea textarea-bordered h-full w-full"
+            placeholder="Type your messaeg here"
+            style="resize: none"
+            v-model="text"
+          ></textarea>
+          <label class="label">
+            <span></span>
+            <span>
+              <button @click="send_message" class="btn btn-sm btn-primary mx-1">
+                Send
+              </button>
+            </span>
+          </label>
+        </div>
+      </div>
+      <div v-else class="container h-full w-full">Select a Chat</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { faker } from "@faker-js/faker";
+import { api } from "@/api";
+import type { IFriend } from "@/interfaces";
+
+export type MessageItemType = {
+  message_id: number;
+  content: string;
+  sendTime: string;
+  seenTime?: string;
+  direction: 1 | 2; // 1: received, 2: sent
+  name?: string;
+  avatar?: string;
+};
+
+const friends = ref<IFriend[] | undefined>();
+const message_list = ref<MessageItemType[] | undefined>();
+const avatar = faker.image.urlLoremFlickr({ width: 64, height: 64 });
+
+const chat_with = ref<string | undefined>();
+const text = ref<string>("");
+
+function select_chat(node_id: string) {
+  chat_with.value = node_id;
+  api.getP2PMessageList(node_id).then((resp) => {
+    message_list.value = resp.data.map((val) => {
+      let name = undefined;
+      let direction = 1;
+      if (Math.random() < 0.5) {
+        let fr = friends.value?.find((f) => f.node_id == val.node_id);
+        name = fr?.remark || val.node_id.toString();
+        direction = 1;
+      } else {
+        name = "Me";
+        direction = 2;
+      }
+      return {
+        message_id: val.message_id,
+        content: val.message,
+        sendTime: val.time,
+        seenTime: "not implemented",
+        direction,
+        name,
+        avatar,
+      } as MessageItemType;
+    });
+  });
+}
+
+function send_message() {
+  console.log(text.value);
+  text.value = "";
+}
+
+api.getFriendList().then((resp) => {
+  friends.value = resp.data;
+});
+</script>
